4559849208092304199322d398c2b82949975b38
Christopher <chrisberry4545@googlemail.com>
2018-02-19 22:42:34 +0000

    Added additional inputs to user form

--- src/models/user-input/user-input.interface.ts
+++ src/models/user-input/user-input.interface.ts
@@ -1,4 +1,7 @@
 export interface UserInputInterface {
+  selectedActivityTypes: string[];
+  selectedCostRange: string;
   selectedFoodTypeIds: string[];
   selectedFlightTimeId: string;
+  selectedTemperatureId: string;
 }
